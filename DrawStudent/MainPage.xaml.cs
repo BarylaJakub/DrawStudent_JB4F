4bed0c5f73131efd2ace3f56689a8e82786e13e0
Jakub <baryla.jakub@gmail.com>
2026-03-30 17:09:05 +0200

    Zapis projektu losowania uczniów (MAUI)

---
+++ DrawStudent/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+
+namespace DrawStudent
+{
+    public partial class MainPage : ContentPage
+    {
+        Random rand = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string GetFilePath(string className)
+        {
+            return Path.Combine(FileSystem.AppDataDirectory, className + ".txt");
+        }
+
+        private void OnSaveClicked(object sender, EventArgs e)
+        {
+            string className = classEntry.Text;
+            string students = studentsEditor.Text;
+
+            if (string.IsNullOrWhiteSpace(className))
+            {
+                resultLabel.Text = "Podaj nazwę klasy!";
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(students))
+            {
+                resultLabel.Text = "Podaj co najmniej 1 ucznia!";
+                return;
+            }
+
+            string path = GetFilePath(className);
+
+            File.WriteAllText(path, students);
+
+            resultLabel.Text = "Zapisano!";
+        }
+
+        private void OnLoadClicked(object sender, EventArgs e)
+        {
+            string className = classEntry.Text;
+
+            if (string.IsNullOrWhiteSpace(className))
+            {
+                resultLabel.Text = "Podaj nazwę klasy!";
+                return;
+            }
+
+            string path = GetFilePath(className);
+
+            if (File.Exists(path))
+            {
+                studentsEditor.Text = File.ReadAllText(path);
+                resultLabel.Text = "Wczytano!";
+            }
+            else
+            {
+                resultLabel.Text = "Nie ma takiej klasy!";
+            }
+        }
+
+        private void OnDrawClicked(object sender, EventArgs e)
+        {
+            string text = studentsEditor.Text;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                resultLabel.Text = "Brak uczniów!";
+                return;
+            }
+
+            string[] students = text
+                .Split(new[] { "\r\n", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            int index = rand.Next(students.Length);
+            resultLabel.Text = "Wylosowano: " + students[index];
+        }
+
+        private void OnClearClicked(object sender, EventArgs e)
+        {
+            classEntry.Text = "";
+            studentsEditor.Text = "";
+            resultLabel.Text = "";
+        }
+    }
+}
---
+++ DrawStudent/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DrawStudent
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
